eb20f5ad05a3546818a8b96e70df49fab892d267
Brian Lach <brianlach72@gmail.com>
2020-12-13 03:45:34 -0500

    theoretical fix when building components on Windows

--- metalibs/toontown/toontown.cxx
+++ metalibs/toontown/toontown.cxx
@@ -7,3 +7,11 @@
 // something to compile when making libtoontown.so in NO_DEFER mode,
 // which generates an empty library that itself links with all the
 // other shared libraries that make up libtoontown.
+
+// This is the one file in this directory which is actually compiled.  It
+// exists just so we can have some symbols and make the compiler happy.
+
+#if defined(_WIN32) && !defined(CPPPARSER) && !defined(LINK_ALL_STATIC)
+__declspec(dllexport)
+#endif
+int toontown;
