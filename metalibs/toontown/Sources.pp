de7c76dfe2af8b681e7a41456cdaa6a4a83f3482
Brian Lach <brianlach72@gmail.com>
2021-10-14 18:37:24 -0400

    toontown stub

--- metalibs/toontown/Sources.pp
+++ metalibs/toontown/Sources.pp
@@ -4,5 +4,9 @@
   #define TARGET toontown
   #define COMPONENT_LIBS pets dnaLoader toontownbase suit
   #define SOURCES toontown.cxx
-  #define BUILDING_DLL BUILDING_TOONTOWN
+  #if $[BUILD_COMPONENTS]
+    #define BUILDING_DLL BUILDING_TOONTOWN_STUB
+  #else
+    #define BUILDING_DLL BUILDING_TOONTOWN
+  #endif
 #end metalib_target
