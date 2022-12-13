3ea79a33e8fcbbcfbd30cac97a16cf24bd7c858f
Erica M. ("Loonatic") <l.oony@aol.com>
2022-12-12 22:50:12 -0600

    Configfiles: Organize toontown.prc

--- src/configfiles/toontown.prc.pp
+++ src/configfiles/toontown.prc.pp
@@ -22,85 +22,98 @@ sound-path      $TTMODELS
 
 plugin-path $TOONTOWN/built/lib
 
+### Window Options ###
+window-title Toontown
+win-origin -2 -2
+#if $[WINDOWS_PLATFORM]
+icon-filename phase_3/models/gui/toontown.ico
+#else
+icon-filename phase_3/models/gui/toontown_mac_icon.rgb
+#endif
+cursor-filename phase_3/etc/toonmono.cur
+show-frame-rate-meter #t
+frame-rate-meter-update-interval 0.01
+
+## Video Options ##
+sync-video #f
 assert-abort #f
+framebuffer-srgb #t
+textures-power-2 none
 
-# Shader Stuff
-
+### Shader Config ###
+# Ambient Occlusion
 ao-debug #f
+ao-blur-normal-factor 0
+ao-blur-depth-factor 0
 
+# Horizon Based Ambient Occlusion:
 hbao-falloff 2.0
 hbao-max-sample-distance 20.0
 hbao-sample-radius 3.5
 hbao-angle-bias 0.564
 hbao-strength 5.0
 
+# Cascaded Shadow Maps:
+csm-distance 200
+csm-sun-distance 400
+csm-fixed-film-size #t
+
+# Postprocess Shaders:
 hdr-enable #t
 bloom-enable #t
 fxaa-enable #t
 ssao-enable #f
 
-ao-blur-normal-factor 0
-ao-blur-depth-factor 0
+# Shadows:
+shadow-depth-bits 24
 
-csm-distance 200
-csm-sun-distance 400
-csm-fixed-film-size #t
+### Animation Config ###
+want-new-anims #f
+hardware-animated-vertices #t
+interpolate-frames #t
 
-# OpenGL stuff
+# This keeps the joint hierarchies for the different LOD's of an Actor separate.
+# Seems to be necessary for the Toons--some of the naked Toons seem to have slightly different skeletons
+# for the different LOD's.
+merge-lod-bundles #f
 
-# PStats stuff
+# this causes pupils, etc. to be generated as separate geometry under
+# their exposed joints, instead of combined with the rest of the head.
+egg-rigid-geometry #t
 
+### Audio Config ###
+audio-dls-file phase_3/etc/gm.dls
+
+### Dev Options ###
+want-dev #f
+
+# check that the quests are internally consistent
+quest-sanity-check #t
+
+# PStats:
 want-pstats #f
 pstats-gpu-timing #f
 
+# DNA/Level Editor:
 level-edit-username lachbr
+load-file-type dna toontown
+dna-preload    phase_4/dna/storage.dna
 
-sync-video #f
-
-want-dev #f
-
-show-frame-rate-meter #t
+### Network Config ###
+fake-playtoken dev
+tt-specific-login #t
+dc-file $TOONTOWN/src/configfiles/toon.dc
 
 # remove this when we integrate Toontown with the Uberdog
 want-uberdog #f
 
-dna-preload    phase_4/dna/storage.dna
-
-want-new-anims #f
-
-framebuffer-srgb #t
-
-shadow-depth-bits 24
-hardware-animated-vertices #t
-
-interpolate-frames #t
-
-textures-power-2 none
-
-load-file-type dna toontown
-
-dc-file $TOONTOWN/src/configfiles/toon.dc
-
-window-title Toontown
-#if $[WINDOWS_PLATFORM]
-icon-filename phase_3/models/gui/toontown.ico
-#else
-icon-filename phase_3/models/gui/toontown_mac_icon.rgb
-#endif
-
 # Set your game server and port to be localhost
 game-server http://localhost:6667
 
-audio-dls-file phase_3/etc/gm.dls
+# Need to turn this on to avoid jerky movement, pirates copes with it differently
+accept-clock-skew #t
 
-tt-specific-login #t
-
-want-news-page #f
-
-fake-playtoken dev
-
-want-magic-words #t
-
+### SSL Config ###
 ssl-certificates $OTP/src/configfiles/certificates.txt
 ssl-certificates $OTP/src/configfiles/gameserver.txt
 
@@ -111,33 +124,19 @@ ssl-certificates $OTP/src/configfiles/gameserver.txt
 expected-ssl-server /O=Disney Enterprises/OU=WDIG/CN=account.toontown.com
 expected-ssl-server /O=Disney Enterprises/OU=WDIG/CN=gameserver.toontown.com
 
-# These two let us also accept the ttown2 certificate.  This is only
-# in the dev environment; it doesn't get published.
+# These two let us also accept the ttown2 certificate.
+# This is only in the dev environment; it doesn't get published.
 ssl-certificates $TOONTOWN/src/configfiles/ttown2.txt
 expected-ssl-server /O=Disney Enterprises/OU=WDIG/CN=ttown2.online.disney.com
 
-# check that the quests are internally consistent
-quest-sanity-check #t
-
-# this causes pupils, etc. to be generated as separate geometry under
-# their exposed joints, instead of combined with the rest of the head.
-egg-rigid-geometry #t
-
-# Toontown is designed for 4/3 aspect ratio, so let's force it to be so
-# Actually, don't turn this on, we support widescreen now.
-# aspect-ratio 1.333333
-
-# This keeps the joint hierarchies for the different LOD's of an Actor
-# separate.  Seems to be necessary for the Toons--some of the naked
-# Toons seem to have slightly different skeletons for the different
-# LOD's.
-merge-lod-bundles #f
-
+### Game Options ###
 # Need to turn on this option to support our broken door triggers.
 early-event-sphere #t
 
-# Need to turn this on to avoid jerky movement, pirates copes with it differently
-accept-clock-skew #t
+want-magic-words #t
+
+# In-game News:
+want-news-page #f
 
 # A local directory to store cached news information in dev.  (In
 # production, this is set to a subdirectory within the current directory).
