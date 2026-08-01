28e6576c60d2033c21d8d1776b8dd41443226463
Bob-by <54867422+Bob-by@users.noreply.github.com>
2026-08-02 01:05:15 +0200

    ihooks are deprecated in python 3

--- src/ai/AIServiceStart.py
+++ src/ai/AIServiceStart.py
@@ -14,8 +14,8 @@ import getopt
 # Initialize ihooks importer On the production servers, we run genPyCode -n
 # meaning no squeeze, so nobody else does this. When we squeeze, the
 # unpacker does this for us and it does not hurt to do in either case.
-import ihooks
-ihooks.install()
+#import ihooks
+#ihooks.install()
 
 from direct.directnotify import RotatingLog
 
