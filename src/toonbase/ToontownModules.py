0b751029e3c80d867c06ce5819a48209b87166d0
Bob-by <54867422+Bob-by@users.noreply.github.com>
2026-08-08 04:01:28 +0200

    Add a few more imports to toontownmodules

--- src/toonbase/ToontownModules.py
+++ src/toonbase/ToontownModules.py
@@ -5,5 +5,11 @@ from otp.otpbase.OTPModules import *
 # Toontown specific modules
 from panda3d.toontown import *
 
-from direct.gui.DirectGuiGlobals import NO_FADE_SORT_INDEX
+# DirectFrames (sometimes) rely on this
+from direct.gui.DirectGuiGlobals import NO_FADE_SORT_INDEX, FOREGROUND_SORT_INDEX, BACKGROUND_SORT_INDEX
+
+# ToonBase.py relies on this
 from panda3d.core import Loader as PandaLoader
+
+# RobotToonManager.py relies on this
+import direct
